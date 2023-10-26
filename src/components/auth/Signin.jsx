e65ca90cb505f06437e1cdb9a9eacd8cc7db268b
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-10-26 14:43:39 +0500

    update adding nickname

--- src/components/auth/Signin.jsx
+++ src/components/auth/Signin.jsx
@@ -1,42 +1,52 @@
-import React from 'react'
-import {signInWithEmailAndPassword} from 'firebase/auth'
-import { useState } from 'react'
-import {auth} from '../../firebase'
-
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
 
 const Signin = () => {
-    const [email,setEm] = useState("")
-    const [pass,setPass]  = useState("")
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const onSignin = (e) => {
-        e.preventDefault
-        signInWithEmailAndPassword(auth,email,pass)
-        .then((useCredenials)=>{
-            console.log(useCredenials)
-        }).catch((error)=>{
-            console.log(error)
-        })
-    }
+  const onSignin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+
+        // Retrieve the user's nickname from the database (or elsewhere)
+        // You can also use local storage or global state for this
+        // Example: const nickname = getUserNicknameFromDatabase(user.uid);
+
+        // Log the user's nickname
+        console.log('User signed in with nickname:', nickname);
+
+        // Perform other actions after successful sign-in
+      })
+      .catch((error) => {
+        console.log('Sign-in error:', error);
+      });
+  };
+
   return (
-    <div className='sign-in-container'>
-        <form onSubmit={onSignin}>
-            <h1>Login</h1>
-            <input
-            type='email'
-            placeholder='enter email'
-            value={email}
-            onChange={(e)=> setEm(e.target.value)}
-            />
-              <input
-            type='password'
-            placeholder='enter password'
-            value={pass}
-            onChange={(e)=> setPass(e.target.value)}
-            />
-            <button>Login</button>
-        </form>
+    <div className="sign-in-container">
+      <form onSubmit={onSignin}>
+        <h1>Login</h1>
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,43 +1,63 @@
-import React from 'react'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
-import { useState } from 'react'
-import {auth} from '../../firebase'
-
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'; // Import the necessary authentication functions
+import { auth } from '../../firebase';
 
 const Signup = () => {
-    const [email,setEm] = useState("")
-    const [pass,setPass]  = useState("")
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [nickname, setNickname] = useState('');
 
-    const onSignUp = (e) => {
-        e.preventDefault();
-        createUserWithEmailAndPassword(auth, email, pass)
-          .then((userCredential) => {
-            console.log(userCredential);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      };
+  const onSignUp = (e) => {
+    e.preventDefault();
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+
+        // Save the user's nickname (and other profile data) to a database
+        // You can also use local storage or global state for this
+        const userProfileData = {
+          nickname,
+          // Add other profile data here if needed
+        };
+
+        // Log the user's nickname
+        console.log('User signed up with nickname:', nickname);
+
+        // Perform actions like saving the user's profile data to a database
+        // or redirecting to a profile setup page
+      })
+      .catch((error) => {
+        console.log('Sign-up error:', error);
+      });
+  };
+
   return (
-    <div className='sign-in-container'>
-        <form onSubmit={onSignUp}>
-            <h1>Create account</h1>
-            <input
-            type='email'
-            placeholder='enter email'
-            value={email}
-            onChange={(e)=> setEm(e.target.value)}
-            />
-              <input
-            type='password'
-            placeholder='enter password'
-            value={pass}
-            onChange={(e)=> setPass(e.target.value)}
-            />
-            <button>Sign up</button>
-        </form>
+    <div className="sign-in-container">
+      <form onSubmit={onSignUp}>
+        <h1>Create account</h1>
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Enter nickname"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        />
+        <button type="submit">Sign up</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
